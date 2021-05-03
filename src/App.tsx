a36d2679309b2daae12eb1abf4395940d3df918b
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-05-03 19:23:25 -0300

    Create the algorithm to calculate IMC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+
+  const handleHeightChange = (event: { target: { value: string | number; }; }) => {
+    setHeight(+event.target.value);
+  }
+
+  const handleWeightChange = (event: { target: { value: string | number; }; }) => {
+    setWeight(+event.target.value);
+  }
+
+  const calculateIMC = (): string => {    
+    const imc: number = weight / ((height / 100) ** 2);
+    const imcToReturn = imc.toFixed(2);
+    
+    if (imc >= 16 && imc <= 16.9) {
+      return `${imcToReturn} kg/m²: Muito abaixo do peso`;
+    }
+
+    if (imc >= 17 && imc <= 18.4) {
+      return `${imcToReturn} kg/m²: Abaixo do peso`;
+    }
+
+    if (imc >= 18.5 && imc <= 24.9) {
+      return `${imcToReturn} kg/m²: Peso normal`;
+    }
+
+    if (imc >= 25 && imc <= 29.9) {
+      return `${imcToReturn} kg/m²: Acima do peso`;
+    }
+
+    if (imc >= 30 && imc <= 34.9) {
+      return `${imcToReturn} kg/m²: Obesidade grau 1`;
+    }
+
+    if (imc >= 35 && imc <= 40) {
+      return `${imcToReturn} kg/m²: Obesidade grau 2`;
+    }
+
+    if (imc > 40) {
+      return `${imcToReturn} kg/m²: Obesidade grau 3`;
+    }
+    
+    return '';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <form className="col s8">
+        <div className="row">       
+          <div className="input-field col s4">
+            <input 
+              id="height" 
+              name="height" 
+              type="number" 
+              className="validate" 
+              min="0" 
+              step="any"
+              value={height}
+              onChange={handleHeightChange}
+            />
+            <label 
+              htmlFor="height"
+              className="active"
+            >Altura (cm)</label>
+          </div>
+          <div className="input-field col s4">
+            <input 
+              id="weight" 
+              name="weight" 
+              type="number" 
+              className="validate" 
+              min="0" 
+              step="any"
+              value={weight}
+              onChange={handleWeightChange}
+            />
+            <label 
+              htmlFor="weight"
+              className="active"
+            >Peso (kg)</label>
+          </div>        
+        </div>
+        <div className="row">
+          <div className="input-field col s8">
+            <input 
+              disabled 
+              name="result" 
+              id="result" 
+              type="text" 
+              className="validate" 
+              value={calculateIMC()}
+            />
+            <label 
+              htmlFor="result"
+              className="active "
+            >IMC</label>
+          </div>        
+        </div>        
+      </form>
     </div>
   );
 }
